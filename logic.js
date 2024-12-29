23b8364e2251b463826891900a25d567f29f3feb
AashvikTyagi <its.aashvik.tyagi@gmail.com>
2024-12-29 16:46:41 +0530

    big update!

--- logic.js
+++ logic.js
@@ -1,20 +1,20 @@
 document.head.insertAdjacentHTML('afterbegin',`
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link rel="icon" type="image/png" href="../favicon.png">
+<link rel="icon" type="image/png" href="/favicon.png">
 <title>aashvik</title>
-<link rel="stylesheet" href="../style.css">
-<link href="https://fonts.googleapis.com/css2?family=Rock+Salt&display=swap" rel="stylesheet">
-<link href="https://fonts.googleapis.com/css2?family=Metal+Mania&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="/style.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Rock+Salt">
+<link rel="stylesheet" href="https://rsms.me/inter/inter.css">
 `);
 
 document.body.insertAdjacentHTML('afterbegin',`
-<a style="text-decoration: none" href="home.html"><header>AashvikT</header></a>
-<nav>
-    <a href="home.html#about">about</a>
-    <a href="home.html#projects">projects</a>
-    <a href="home.html#todo">todo</a>
-</nav>
+<div class="header">
+    <a style="font-weight: 800; text-decoration: none;" href="/index.html">AashvikT</a> :
+    <a href="#about">about</a> /
+    <a href="#projects">projects</a> /
+    <a href="#todo">todo</a>
+</div>
 `);
 
 document.body.insertAdjacentHTML('beforeend',`
@@ -25,24 +25,24 @@ document.body.insertAdjacentHTML('beforeend',`
     <select id="color-dd" onchange="changePalette()">
         <option>pulpe</option> <!--purple/green-->
         <option>b/o/w</option> <!--black on white-->
-        <option>w/o/b</option> <!--white on black-->
-        <option>clbd1</option> <!--colorblind friendly 1-->
-        <option>clbd2</option> <!--colorblind friendly 2-->
+        <option selected='selected'>w/o/b</option> <!--white on black-->
+        <option>clbd1</option> <!--colorblind-friendly 1-->
+        <option>clbd2</option> <!--colorblind-friendly 2-->
         <option>bnshk</option> <!--banana shake-->
         <option>mnion</option> <!--minion-->
-        <option selected='selected'>tensy</option> <!--teensy 4.1 green-->
+        <option>tensy</option> <!--teensy 4.1 green-->
         <option>minty</option> <!--grey mint green-->
         <option>invyc</option> <!--dark hacker news-->
         <option>hcknw</option> <!--hacker news-->
-        <option>lspcl</option> <!--la special-->
-    </select>]
-    <br>
-    <a href="https://www.youtube.com/@AashvikT">youtube</a> /
-    <a href="https://github.com/AashvikTyagi">github</a> /
-    <a href="https://stackoverflow.com/users/17048834/aashvikt">stack</a> /
-    <a href="mailto:its.aashvik.tyagi@gmail.com">email</a> /
-    <a href="https://www.reddit.com/user/aashviktyagi/">reddit</a> /
-    <a href="http://discordapp.com/users/aashvikt">discord</a>
+        <option>lspcl</option> <!--la speciale-->
+        <option>gruvb</option> <!--gruvbox dark-->
+    </select>].
+    <a href="https://www.youtube.com/@AashvikT">yt</a> /
+    <a href="https://github.com/AashvikTyagi">gh</a> /
+    <a href="https://stackoverflow.com/users/17048834/aashvikt">so</a> /
+    <a href="mailto:its.aashvik.tyagi@gmail.com">mail</a> /
+    <a href="https://www.reddit.com/user/aashviktyagi/">red</a> /
+    <a href="http://discordapp.com/users/aashvikt">disc</a>
 </footer>
 `);
 
@@ -58,11 +58,12 @@ const colorPalettes = [
     ["#ffffff","#202020","#28d28c","#292929"],
     ["#202020","#f6f6ef","#ff6600","#ffffff"],
     ["#f6f6ef","#202020","#ff6600","#1c1d1f"],
-    ["#070707","#00a9a6","#fbf1c7","#de423e"]
+    ["#070707","#00a9a6","#fbf1c7","#de423e"],
+    ["#1d2021","#fbf1c7","#458588","#b8bb26"],
 ]
 
-const colors=['--bck-color','--txt-color','--hgl-color','--link-color']
+const colors = ['--bck-color','--txt-color','--hgl-color','--link-color']
 function changePalette(){
     palette = colorPalettes[document.querySelector('#color-dd').selectedIndex];
-    for(let i=0;i<colorPalettes[0].length;i++)document.body.style.setProperty(colors[i],palette[i]);
+    for (let i=0;i<colorPalettes[0].length;i++) document.body.style.setProperty(colors[i],palette[i]);
 }
