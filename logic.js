078532213aec560c76e0105ad7e55c0b2706c27c
AashvikTyagi <its.aashvik.tyagi@gmail.com>
2024-12-30 12:05:31 +0530

    fixed homepage link

--- logic.js
+++ logic.js
@@ -10,7 +10,7 @@ document.head.insertAdjacentHTML('afterbegin',`
 
 document.body.insertAdjacentHTML('afterbegin',`
 <div class="header">
-    <a style="font-weight: 800; text-decoration: none;" href="/index.html">AashvikT</a> :
+    <a style="font-weight: 800; text-decoration: none;" href="/">AashvikT</a> :
     <a href="#about">about</a> /
     <a href="#projects">projects</a> /
     <a href="#todo">todo</a>
