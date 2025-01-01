467f01d29286389e68f9ee4a804633fdb4e708e1
AashvikTyagi <its.aashvik.tyagi@gmail.com>
2025-01-01 13:39:29 +0530

    added title in head

--- logic.js
+++ logic.js
@@ -4,7 +4,6 @@ document.head.insertAdjacentHTML('afterbegin',`
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link rel="icon" type="image/png" href="/favicon.png">
-<title>aashvik</title>
 <link rel="stylesheet" href="/style.css">
 <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
 `);
